b8ad097831e5cbdd4d59c838f09314201835549c
sfmunera10 <sf.munera1130@gmail.com>
2025-02-27 15:01:50 -0500

    EmailParser feature working

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SesSnsEventMapperModule } from './ses-sns-event-mapper/ses-sns-event-mapper.module';
 import { EmailParserModule } from './email-parser/email-parser.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [SesSnsEventMapperModule, EmailParserModule],
+  imports: [
+    SesSnsEventMapperModule,
+    EmailParserModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      renderPath: '/public',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/email-parser/email-parser.controller.ts
+++ src/email-parser/email-parser.controller.ts
@@ -1,34 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import { EmailParserService } from './email-parser.service';
-import { CreateEmailParserDto } from './dto/create-email-parser.dto';
-import { UpdateEmailParserDto } from './dto/update-email-parser.dto';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { AttachmentStream, MailParser, MessageText } from 'mailparser';
+import { Response } from 'express';
 
 @Controller('email-parser')
 export class EmailParserController {
   constructor(private readonly emailParserService: EmailParserService) {}
-
-  @Post()
-  create(@Body() createEmailParserDto: CreateEmailParserDto) {
-    return this.emailParserService.create(createEmailParserDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.emailParserService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.emailParserService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEmailParserDto: UpdateEmailParserDto) {
-    return this.emailParserService.update(+id, updateEmailParserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.emailParserService.remove(+id);
+  @Get(':resource')
+  getEmailAttatchment(
+    @Param('resource') emailUrlOrPath: string,
+    @Res() res: Response,
+  ) {
+    createReadStream(join(process.cwd(), 'emails', emailUrlOrPath))
+      .pipe(new MailParser())
+      .on('data', (data: AttachmentStream | MessageText) => {
+        if (data.type === 'attachment') {
+          data.content.pipe(res);
+          data.content.on('end', () => data.release());
+        }
+      });
   }
 }
