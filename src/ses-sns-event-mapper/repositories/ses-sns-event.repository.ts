2c510f5c4e58128ae8d2e96b4d9b8dff270d9ea6
Santiago Múnera <sf.munera1130@gmail.com>
2025-02-27 16:20:29 -0500

    Feature ses sns event mapper (#4)
    * SesSnsEventMapper feature working
    
    * Dependency injection improvements for SesSnsEventMapper

--- src/ses-sns-event-mapper/repositories/ses-sns-event.repository.ts
+++
@@ -1,6 +0,0 @@
-import { MappedSesSnsEvent } from '../interfaces/mapped-ses-sns-event.interface';
-import { SesSnsEvent } from '../interfaces/ses-sns-event.interface';
-
-export interface SesSnsEventRepository {
-  createMappedSeSSnsEvent(sesSnsEvent: SesSnsEvent): MappedSesSnsEvent;
-}
--- src/ses-sns-event-mapper/ses-sns-event-mapper.module.ts
+++ src/ses-sns-event-mapper/ses-sns-event-mapper.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { SesSnsEventMapperService } from './ses-sns-event-mapper.service';
+import { SesSnsEventMapperService } from './providers/ses-sns-event-mapper.service';
 import { SesSnsEventMapperController } from './ses-sns-event-mapper.controller';
+import { DynamicObjectMapperService } from './providers/dynamic-object-mapper.service';
 
 @Module({
   controllers: [SesSnsEventMapperController],
-  providers: [SesSnsEventMapperService],
+  providers: [SesSnsEventMapperService, DynamicObjectMapperService],
 })
 export class SesSnsEventMapperModule {}
