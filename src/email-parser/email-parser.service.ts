16e1dd0e01ec244240ba03615f50c2d9405e176c
Santiago Múnera <sf.munera1130@gmail.com>
2025-02-27 15:02:12 -0500

    EmailParser feature working (#3)

--- src/email-parser/email-parser.service.ts
+++ src/email-parser/email-parser.service.ts
@@ -1,26 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { CreateEmailParserDto } from './dto/create-email-parser.dto';
-import { UpdateEmailParserDto } from './dto/update-email-parser.dto';
 
 @Injectable()
 export class EmailParserService {
-  create(createEmailParserDto: CreateEmailParserDto) {
-    return 'This action adds a new emailParser';
-  }
-
-  findAll() {
-    return `This action returns all emailParser`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} emailParser`;
-  }
-
-  update(id: number, updateEmailParserDto: UpdateEmailParserDto) {
-    return `This action updates a #${id} emailParser`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} emailParser`;
-  }
+  getEmailAttatchment() {}
 }
